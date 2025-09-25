c8e874b7e0d0d75a80c66794886bc96038475e38
Shoichiinaba <shoichiinaba01@gmail.com>
2025-09-25 12:06:18 +0700

    update web ke hipmi kota SMG

--- application/views/page_admin/login/login.php
+++ application/views/page_admin/login/login.php
@@ -7,7 +7,7 @@
     <meta name="viewport"
         content="width=device-width, initial-scale=1.0, user-scalable=no, minimum-scale=1.0, maximum-scale=1.0" />
 
-    <title>Hipmi Kab. Semarang | Login</title>
+    <title>Hipmi Kota Semarang | Login</title>
 
     <meta name="description" content="" />
 
